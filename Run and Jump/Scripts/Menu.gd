06d6fe6baa32891f42b4ef3692a701bb057b4f0c
unknown <fabiodinnocenzio@gmail.com>
2021-03-29 11:04:54 +0200

    main menu and background of game

---
+++ Run and Jump/Scripts/Menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+onready var play = $background/MarginContainer/VBoxContainer/VBoxContainer/Play
+onready var settings = $background/MarginContainer/VBoxContainer/VBoxContainer/Settings
+onready var customise = $background/MarginContainer/VBoxContainer/VBoxContainer/Customise
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://Scenes/Game.tscn")
+	pass 
+
+func _on_Setting_pressed():
+	pass 
+
+func _on_Customise_pressed():
+	pass 
